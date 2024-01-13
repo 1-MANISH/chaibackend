bb8e96e3e24c4eee0e9ce8012a1524b7d8bc8282
manish_patidar <shanimpatidar@gmail.com>
2024-01-13 16:26:34 +0530

    Added Another Url

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const app = express() // using that created a varable like math
 
 const port = process.env.PORT
 
+const jsonData = {
+    name:"Manish",
+    email:"man@gmail.com",
+    occupation:"Student",
+    age:21
+}
+
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
@@ -22,6 +29,11 @@ app.get('/signup',(req,res)=>{
     res.send('Signup Request - Handler')
 })
 
+app.get('/about',(req,res)=>{
+    res.json.send(jsonData)
+})
+
 app.listen(port,()=>{
     console.log(`Listening On Port ${port}`);
-})
+})                                                                                                           
+
